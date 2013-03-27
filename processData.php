5ccd8d981f14b2373942508e1a285dcd2ef2eb75
jason3e7 <jason3e7@gmail.com>
2013-03-27 23:28:06 +0800

    Fix processData correct

--- processData.php
+++ processData.php
@@ -3,11 +3,25 @@ require_once("../Classes/PHPExcel.php");
 require_once '../Classes/PHPExcel/IOFactory.php';
 $reader = PHPExcel_IOFactory::createReader('Excel5');
 
-$geneAssociationExcel = $reader->load("upload/file1_middle.xls");
+$geneAssociationExcel = $reader->load("upload/file1_full.xls");
 $geneAssociationSheet = $geneAssociationExcel->getSheet(0);
+$eisenExcel = $reader->load("upload/file2_full.xls");
+$eisenSheet = $eisenExcel->getSheet(0);
+
+$highestRow = $eisenSheet->getHighestRow();
+var_dump("block:1");
+$mappingArray = array();
+for ($row = 2; $row <= $highestRow; $row++) {
+	$Gene_Name = $eisenSheet->getCellByColumnAndRow(0, $row)->getValue();
+	$mappingArray[$Gene_Name][0] = true; 
+	
+	for ($column = 1; $column <= 80; $column++) {
+		$mappingArray[$Gene_Name][$column] = $eisenSheet->getCellByColumnAndRow($column, $row)->getValue();
+	}
+}
 
 $highestRow = $geneAssociationSheet->getHighestRow();
-
+var_dump("block:2");
 $sourceArray = array();
 for ($row = 2; $row <= $highestRow; $row++) {
 	if ($geneAssociationSheet->getCellByColumnAndRow(1, $row)->getValue() !== 'C') {
@@ -15,6 +29,10 @@ for ($row = 2; $row <= $highestRow; $row++) {
 	}
 	$GO_ID = $geneAssociationSheet->getCellByColumnAndRow(0, $row)->getValue();
 	$Gene_Name = strtok($geneAssociationSheet->getCellByColumnAndRow(2, $row)->getValue(), "|");
+
+	if(isset($mappingArray[$Gene_Name][0]) === false) {
+		continue;
+	}
 
 	if (isset($sourceArray[$GO_ID][$Gene_Name]) === false) {
 		$sourceArray[$GO_ID][$Gene_Name] = true;
@@ -30,7 +48,7 @@ for ($row = 2; $row <= $highestRow; $row++) {
 
 $objPHPExcel = new PHPExcel();
 
-$objPHPExcel->getProperties()->setCreator("測試作者")//作者
+$objPHPExcel->getProperties()->setCreator("jason3e7")//作者
    ->setLastModifiedBy("測試修改者")//最後修改者
    ->setTitle("測試標題")//標題
    ->setSubject("測試主旨")//主旨
@@ -40,55 +58,28 @@ $objPHPExcel->getProperties()->setCreator("測試作者")//作者
 
 $objPHPExcel->setActiveSheetIndex(0);
 $excel_line = 1;
-
+var_dump("block:3");
 foreach ($sourceArray as $key => $GO_ID) {
 	if ($GO_ID['count'] > 30) {
 		foreach ($GO_ID as $Gene_Name => $value) {
 			if ($Gene_Name != 'count') {	
 				$objPHPExcel->getActiveSheet()->setCellValue("A{$excel_line}", $key);
 				$objPHPExcel->getActiveSheet()->setCellValue("B{$excel_line}", $Gene_Name);
+				for ($column = 1; $column <= 80; $column++) {
+					$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($column + 1, $excel_line, $mappingArray[$Gene_Name][$column]);
+				}
 				$excel_line++;
 			}
 		}
 	}
 }
 
-
 //Excel檔名
 $filename = "Report_".time().".xls";
 
+var_dump("block:4");
 //Excel下載檔
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');//20003格式
 $objWriter->save('output/'.$filename);
 
-/*
-while (current($sourceArray)) {
-	echo key($sourceArray).'<br />';
-	while (current($sourceArray[key($sourceArray)])) {
-        echo key($sourceArray[key($sourceArray)]).'<br />';
-		next($sourceArray[key($sourceArray)]);
-    }
-    next($sourceArray);
-}
-*/
-
-/*
-while (current($sourceArray)) {
-	echo key($sourceArray).'<br />';
-	while (current($sourceArray[key($sourceArray)])) {
-        echo key($sourceArray[key($sourceArray)]).'<br />';
-		next($sourceArray[key($sourceArray)]);
-    }
-    next($sourceArray);
-}
-*/
-
-/*
-foreach ($sourceArray as $key => $value) {
-	//if ($value['count'])
-	echo $key.$value['count'].'<br />';
-}
-*/
-
-//var_dump(key($sourceArray));
 ?>
